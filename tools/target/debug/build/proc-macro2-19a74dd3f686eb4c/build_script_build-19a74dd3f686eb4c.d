6f21d2d7d339ce90d6df273f1d8f2a593b2c210b
SpaghettiMan <alessioamiri182@gmail.com>
2023-12-06 16:53:51 +0100

    Basic structure

---
+++ tools/target/debug/build/proc-macro2-19a74dd3f686eb4c/build_script_build-19a74dd3f686eb4c.d
@@ -0,0 +1,5 @@
+C:\Users\aless\Documents\UNI\rust\TheRustOfUsTools\tools\target\debug\build\proc-macro2-19a74dd3f686eb4c\build_script_build-19a74dd3f686eb4c.exe: C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.70\build.rs
+
+C:\Users\aless\Documents\UNI\rust\TheRustOfUsTools\tools\target\debug\build\proc-macro2-19a74dd3f686eb4c\build_script_build-19a74dd3f686eb4c.d: C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.70\build.rs
+
+C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.70\build.rs:
---
+++ tools/target/debug/build/serde-3cb72809ccba4128/build_script_build-3cb72809ccba4128.d
@@ -0,0 +1,5 @@
+C:\Users\aless\Documents\UNI\rust\TheRustOfUsTools\tools\target\debug\build\serde-3cb72809ccba4128\build_script_build-3cb72809ccba4128.exe: C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.193\build.rs
+
+C:\Users\aless\Documents\UNI\rust\TheRustOfUsTools\tools\target\debug\build\serde-3cb72809ccba4128\build_script_build-3cb72809ccba4128.d: C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.193\build.rs
+
+C:\Users\aless\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.193\build.rs:
